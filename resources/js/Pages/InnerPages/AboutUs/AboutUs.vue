d1b4c91736a3d7da7976be02ebbac8877cd6f4f3
gokselkoc00 <goksel.koc@invekor.com>
2023-10-17 17:26:03 +0300

    image and aboutus references pages  updates

--- resources/js/Pages/InnerPages/AboutUs/AboutUs.vue
+++ resources/js/Pages/InnerPages/AboutUs/AboutUs.vue
@@ -5,12 +5,22 @@
             <img style="max-height: 400px; width: 100%;" :src="hero" class="img-fluid">
 
         </div>
-        <div class="container">
 
-            <div class="col-md-12 row" style="padding-top: 80px; padding-bottom: 70px;  text-align: center;">
-                <div class="col-12">
-                    <h1>Hakkımızda</h1>
+        <div class="container">
+            <div class="row" style="margin-top: 80px">
+                <div class="col-md-12" style="display: flex; align-items: center; justify-content: center">
+                    <div class=" ">
+                        <Transition name="slide-title">
+                            <h1 class="header-title" v-if="data?.head_title">
+                                {{ data?.head_title }}
+                            </h1>
+                        </Transition>
+                    </div>
                 </div>
+            </div>
+            <div class="custom-carousel-caption d-none d-md-block mt-5"></div>
+            <div class="col-md-12 row" style="padding-top: 80px; padding-bottom: 70px;  text-align: center;">
+               
                 <div class="row mt-5" >
                 <div class="col-md-12 col-lg-6 " style="
                     display: flex;
@@ -73,7 +83,41 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+
+.header-title {
+    display: block;
+    align-items: center;
+    position: relative;
+    color: #222;
+    white-space: nowrap;
+    /* padding-top: 2rem;
+  padding-bottom: 2rem; */
+    /* border-top: 3px solid #47b2e4 !important;
+  border-bottom: 3px solid #47b2e4 !important; */
+}
+
+.header-title::after {
+    margin-left: 0.83rem;
+
+    margin-bottom: 0.9rem !important;
+    content: '';
+    width: 50px;
+    height: 2px;
+    background: #2186c2;
+    display: inline-block;
+}
+
+.header-title::before {
+    margin-right: 0.83rem;
+    margin-bottom: 0.9rem !important;
+    content: '';
+    width: 50px;
+    height: 2px;
+    background: #2186c2;
+    display: inline-block;
+}
+
 @media(max-width: 992px) {
     .text-content {
         margin-top: 2rem;
--- resources/js/Pages/InnerPages/References/References.vue
+++ resources/js/Pages/InnerPages/References/References.vue
@@ -1,121 +1,158 @@
 <template>
-  <Head title="Hakkımızda"></Head>
-  <Layout>
-    <div class="row">
+    <Head title="Referanslarımız"></Head>
+    <Layout>
+        <div class="row">
             <img style="max-height: 400px; width: 100%;" :src="heroImage" class="img-fluid">
 
         </div>
-    <div class="container">
-      <div
-        class="row"
-        style="padding-top: 80px; padding-bottom: 70px; text-align: center"
-      >
-        <div class=" col-md-12 col-lg-12">
-          <h1>
-            {{ title?.title }}
-          </h1>
-          <p class="mt-5">{{ title?.text }}</p>
-        </div>
-        <div class="row w-100 mt-5" style="margin-left: 0px; margin-right: 0px">
-          <div
-            v-for="(item, index) in images"
-            :key="index"
-            class="col-xl-3 col-lg-4 col-md-6 d-flex align-items-stretch"
-          >
-            <div class="icon-box referances-item"             style="width: 100%;"
->
-              <div class="referances-icon">
-                <div>
-                  <img class="custom-logo-img img-fluid" :src="item?.img_url" />
-                  <h6 class="text-center" style="padding-top: 1.5rem">
-                    <Link
-                      style="color: black;"
-                      :href="route('references-inner.index')"
-                      >{{ item?.title }}</Link
-                    >
-                  </h6>
+        <div class="container">
+            <div class="row" style="padding-top: 80px; padding-bottom: 70px; text-align: center">
+                <div class="col-md-12" style="display: flex; align-items: center; justify-content: center">
+                    <div class=" ">
+                        <Transition name="slide-title">
+                            <h1 class="header-title" v-if="title?.title">
+                                {{ title?.title }}
+                            </h1>
+
+                        </Transition>
+                        <Transition name="slide-title">
+                            <p class="mt-5" v-if="title?.text">{{ title?.text }}</p>
+
+
+                        </Transition>
+                    </div>
                 </div>
-                <!-- <div class="row" style="padding-top: 1rem">
+
+                <div class="row w-100 mt-5" style="margin-left: 0px; margin-right: 0px">
+                    <div v-for="(item, index) in images" :key="index"
+                        class="col-xl-3 col-lg-4 col-md-6 d-flex align-items-stretch">
+                        <div class="icon-box referances-item" style="width: 100%;">
+                            <div class="referances-icon">
+                                <div>
+                                    <img class="custom-logo-img img-fluid" :src="item?.img_url" />
+                                    <h6 class="text-center" style="padding-top: 1.5rem">
+                                        <Link style="color: black;" :href="route('references-inner.index')">{{ item?.title
+                                        }}</Link>
+                                    </h6>
+                                </div>
+                                <!-- <div class="row" style="padding-top: 1rem">
 
                   </div> -->
-              </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row w-100 justify-content-center">
+                    <p class="mt-5">{{ title?.text_footer }}</p>
+                </div>
             </div>
-          </div>
         </div>
-        <div class="row w-100 justify-content-center">
-            <p class="mt-5" >{{ title?.text_footer }}</p>
-        </div>
-      </div>
-    </div>
-  </Layout>
+    </Layout>
 </template>
 
-  <script>
+<script>
 import axios from "axios";
 import Layout from "../../Shared/Layout.vue";
 
 export default {
-  data() {
-    return {
-      images: {},
-      title: {},
-      heroImage:""
-    };
-  },
-  components: {
-    Layout,
-  },
-  mounted() {
-    this.getData();
-  },
-  methods: {
-    async getData() {
-      try {
-        axios
-          .get(route("references-inner.data"))
-          .then((response) => {
-            this.images = response.data.images;
-            this.title = response.data.title;
-            this.heroImage = response.data.heroImage;
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-      } catch (error) {
-        console.error(error);
-      }
+    data() {
+        return {
+            images: {},
+            title: {},
+            heroImage: ""
+        };
     },
-  },
+    components: {
+        Layout,
+    },
+    mounted() {
+        this.getData();
+    },
+    methods: {
+        async getData() {
+            try {
+                axios
+                    .get(route("references-inner.data"))
+                    .then((response) => {
+                        this.images = response.data.images;
+                        this.title = response.data.title;
+                        this.heroImage = response.data.heroImage;
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                    });
+            } catch (error) {
+                console.error(error);
+            }
+        },
+    },
 };
 </script>
 
-  <style>
+<style scoped>
 @media (max-width: 992px) {
-  .text-content {
-    margin-top: 2rem;
-  }
+    .text-content {
+        margin-top: 2rem;
+    }
 }
 
 .icon-box {
     margin-bottom: 16px;
-  padding: 2rem;
-  transition: all ease-in-out 0.4s;
-  min-width: 285px;
-  text-align: center;
+    padding: 2rem;
+    transition: all ease-in-out 0.4s;
+    min-width: 285px;
+    text-align: center;
 }
-.icon-box:hover{
+
+.icon-box:hover {
     transform: translateY(-10px);
 }
+
 .custom-logo-img {
-  max-height: 100px;
-  min-height: 100px;
+    max-height: 100px;
+    min-height: 100px;
 }
+
 .referances-item {
     display: flex;
-  align-content: center;
-  justify-content: center;
-  flex-wrap: wrap;
-  border: #ccc solid 1px;
-  border-radius: 4px;
+    align-content: center;
+    justify-content: center;
+    flex-wrap: wrap;
+    border: #ccc solid 1px;
+    border-radius: 4px;
+}
+
+
+.header-title {
+    display: block;
+    align-items: center;
+    position: relative;
+    color: #222;
+    white-space: nowrap;
+    /* padding-top: 2rem;
+  padding-bottom: 2rem; */
+    /* border-top: 3px solid #47b2e4 !important;
+  border-bottom: 3px solid #47b2e4 !important; */
+}
+
+.header-title::after {
+    margin-left: 0.83rem;
+
+    margin-bottom: 0.9rem !important;
+    content: '';
+    width: 50px;
+    height: 2px;
+    background: #2186c2;
+    display: inline-block;
+}
+
+.header-title::before {
+    margin-right: 0.83rem;
+    margin-bottom: 0.9rem !important;
+    content: '';
+    width: 50px;
+    height: 2px;
+    background: #2186c2;
+    display: inline-block;
 }
 </style>
